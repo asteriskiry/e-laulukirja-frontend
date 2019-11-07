41ee8286c7419d3486ae3e22f99728a85f6ccd1d
Maks Turtiainen <maks.turtiainen@lvsbrokers.com>
2019-11-07 17:51:14 +0200

    Start

---
+++ src/views/HomeView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MainComponent from '../components/main/MainComponent.js';
+
+class HomeView extends Component {
+  render() {
+    return (
+      <MainComponent>
+        <h1>E-laulukirja</h1>
+      </MainComponent>
+    );
+  }
+}
+
+export default HomeView;
