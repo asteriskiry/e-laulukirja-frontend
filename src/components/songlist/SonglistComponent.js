4f4feed6f474f7d6fe59cd988dd18eb4e03fabb7
Jaakko Kittilä <jmkitt@utu.fi>
2020-01-18 21:40:55 +0200

    Lisätty oikea laulukirja ja paranneltu ulkoasua

--- src/components/songlist/SonglistComponent.js
+++ src/components/songlist/SonglistComponent.js
@@ -16,35 +16,40 @@ class SonglistComponent extends Component {
     this.setState({search: event.target.value})
 }
   render() {
-    var filtered = this.state.songs.filter(song => this.state.search.toLowerCase() === song.name.substring(0, this.state.search.length).toLowerCase())
+    var filtered = this.state.songs.filter(song => this.state.search.toLowerCase() === song.title.substring(0, this.state.search.length).toLowerCase())
    
    
-    const songs = filtered.map((song, key) => (
-    <li key={song.number}><a href="#">{song.name}</a></li>
-     ));
+    const songs = <table id="songlist">
+      <tr><th colSpan="2"><input id="search" placeholder="Etsi..." onChange={this.handleChange}></input></th></tr>
+      <tr id="toprow"><th>Laulu</th><th>Numero</th></tr>{filtered.map((song, key) => (
+      <tr id="tablerow" key={song.title}><th>{song.title}</th><th>{song.number}</th></tr>
+     ))}</table>
     
     
     return (
       <div id="main">
-        <input onChange={this.handleChange}></input>
-        <ul>
-          {songs}
-        </ul>
+        
+        
+        {songs}
+        
       </div>
         )
   }
   async componentDidMount() {
     try {
-      let songs = await axios.get('songs.json', {
+      let songs = await axios.get('laulukirjaV3.json', {
         headers: {
           'Content-Type': 'application/json',
         },
       });
 
+      let songdata = songs.data
+      let sortedSongs= songdata.sort((a, b) => (a.number > b.number) ? 1 : -1)
+
       this.setState({
         ...this.state,
         ...{
-          songs: songs.data,
+          songs: sortedSongs,
         },
       });
     } catch (e) {
